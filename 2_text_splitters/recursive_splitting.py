8d9660d268fd43884c1c9988141076c3f4eb045c
vicky <vicky210490@gmail.com>
2026-08-26 15:59:22 +0530

    implement recursive splitting for docs

--- 2_text_splitters/recursive_splitting.py
+++ 2_text_splitters/recursive_splitting.py
@@ -1,3 +1,4 @@
+from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 text = """Hi how are you
@@ -63,3 +64,16 @@ print(f"No. of chunks: {len(chunk2)}")
 # 'Researchers are working on making AI systems more transparent and explainable.'
 # ]
 # No. of chunks: 9
+
+# DOCUMENT SPLITTING RECURSIVE
+text_list = [text, example_text]
+
+docs = [Document(page_content=text) for text in text_list]
+
+chunk3 = r2splitter.split_documents(docs)
+
+print(chunk3)
+print(f"No. of chunks: {len(chunk3)}")
+
+# [Document(metadata={}, page_content="Hi how are you\nmy name is rahul\n\ni am teaching RAG\nwe're learning about RAG"), Document(metadata={}, page_content='Artificial intelligence is transforming technology and shaping the future.'), Document(metadata={}, page_content='Machine learning algorithms are becoming more sophisticated every day. \nDeep learning models can now process vast amounts of data efficiently.'), Document(metadata={}, page_content='Natural language processing has made significant strides in recent years.\nTransformers architecture revolutionized the field in 2017.'), Document(metadata={}, page_content='Models like GPT and BERT have set new benchmarks.'), Document(metadata={}, page_content='Computer vision systems can now identify objects with remarkable accuracy.\nConvolutional neural networks excel at image recognition tasks.'), Document(metadata={}, page_content='Self-driving cars rely heavily on advanced computer vision.'), Document(metadata={}, page_content='The impact of AI extends across multiple industries including healthcare, finance, and transportation.'), Document(metadata={}, page_content='Ethical considerations around AI development are becoming increasingly important.'), Document(metadata={}, page_content='Researchers are working on making AI systems more transparent and explainable.')]
+# No. of chunks: 10
