655f44ee7500f27e9f8a91451be5fa49dede562c
vicky <vicky210490@gmail.com>
2026-09-16 16:04:09 +0530

    add is relevant node

--- ch_11_self_rag/nodes.py
+++ ch_11_self_rag/nodes.py
@@ -6,6 +6,7 @@ class Node(StrEnum):
     DECIDE_RETRIEVAL = "decide_retrieval"
     GENERATE_DIRECT = "generate_direct"
     RETRIEVE = "retrieve"
+    IS_RELEVANT = "is_relevant_node"
 
 
 # Define your Routing Decisions as typed constants
