9fa5b9b12f391a684b88824567dcf96caa9a2b4f
vicky <vicky210490@gmail.com>
2026-09-22 00:05:19 +0530

    add new nodes and route verdicts

--- ch_12_agentic_rag/nodes.py
+++ ch_12_agentic_rag/nodes.py
@@ -4,3 +4,9 @@ from enum import StrEnum
 class Node(StrEnum):
     RETRIEVE = "retrieve"
     GENERATE_RESPONSE = "generate_response"
+    DECIDE_RETRIEVAL = "decide_retrieval"
+
+
+class RouteVerdict(StrEnum):
+    NEED_RETRIEVAL = "need_retrieval"
+    GENERATE_DIRECT = "direct_generate"
