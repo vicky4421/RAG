02fdb630873546e0893063d32d39b9be1f446f0a
vicky <vicky210490@gmail.com>
2026-09-22 16:13:15 +0530

    add new nodes

--- ch_12_agentic_rag/nodes.py
+++ ch_12_agentic_rag/nodes.py
@@ -5,8 +5,13 @@ class Node(StrEnum):
     RETRIEVE = "retrieve"
     GENERATE_RESPONSE = "generate_response"
     DECIDE_RETRIEVAL = "decide_retrieval"
+    AGENT = "agent"
+    TOOL = "tool"
+    TOOL_OUTPUT = "tool_output"
 
 
 class RouteVerdict(StrEnum):
     NEED_RETRIEVAL = "need_retrieval"
     GENERATE_DIRECT = "direct_generate"
+    TOOLS_REQUIRED = "tools_required"
+    TOOLS_COMPLETE = "tools_complete"
--- ch_12_agentic_rag/step_2.py
+++ ch_12_agentic_rag/step_2.py
@@ -1,1 +1,47 @@
+# define state
+import operator
+from typing import Annotated
 
+from langchain_core.documents import Document
+from langchain_core.tools import tool
+from langgraph.graph import MessagesState
+
+from ch_12_agentic_rag.step_0 import vector_store
+from utils.logger import get_logger
+
+logger = get_logger()
+
+logger.info("Agentic RAG step_2 execution started.")
+
+
+class State(MessagesState):
+    query: str
+    retrieved_docs: Annotated[list[Document], operator.add]
+    context: Annotated[str, operator.add]
+    needs_retrieval: bool
+
+
+@tool(response_format="content_and_artifact")
+def vector_store_search(query: str, k: int = 3) -> tuple[str, list[Document]]:
+    logger.info("Tool called: vector_store_search")
+
+    """
+    Search the vector store for relevant document passages.
+    Adjust k (default 3) to retrieve more or fewer passages.
+    """
+    retriever = vector_store.as_retriever(search_kwargs={"k": k})
+    docs = retriever.invoke(input=query)
+
+    context = "\n\nVector Store Results\n\n" + "\n\n".join(d.page_content for d in docs)
+
+    # due to response format 'content_and_artifacts' we can return content=context and artifact=docs
+    return context, docs
+
+@tool(response_format="content_and_artifact")
+def web_search(query: str, max_results: int = 3) -> tuple[str, list[Document]]:
+    logger.info('Tool called: web search')
+    '''
+    Search the web for current or real time information.
+    Adjust max_results (default 3) to control how many results are returned.
+    '''
+    client = 
