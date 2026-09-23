a5ca18424f68449c55145d740fdf877a04ab82ff
vicky <vicky210490@gmail.com>
2026-09-24 00:27:08 +0530

    add new nodes and route verdicts

--- ch_12_agentic_rag/nodes.py
+++ ch_12_agentic_rag/nodes.py
@@ -9,6 +9,8 @@ class Node(StrEnum):
     TOOL = "tool"
     TOOL_OUTPUT = "tool_output"
     INCREMENT_TOOL_CALL = "increment_tool_call"
+    EVALUATE_DOCS = "evaluate_docs"
+    REWRITE_QUERY = "rewrite_query"
 
 
 class RouteVerdict(StrEnum):
@@ -17,3 +19,7 @@ class RouteVerdict(StrEnum):
     TOOLS_REQUIRED = "tools_required"
     TOOLS_COMPLETE = "tools_complete"
     TOOL_CALL_LIMIT_REACHED = "tool_call_limit_reached"
+    DOCS_ARE_RELEVANT = "docs_are_relevant"
+    DOCS_ARE_NOT_RELEVANT = "docs_are_not_relevant"
+    DO_NOT_REWRITE = "don't_rewrite"
+    NEED_TO_REWRITE = "need_to_rewrite"
